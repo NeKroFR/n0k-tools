faac09c7baaf2610e7c91752b7742d865b353f63
NeKroFR <fxgaming73@gmail.com>
2021-06-16 15:13:23 +0200

    installing metasploit

--- n0ktools.sh
+++ n0ktools.sh
@@ -41,7 +41,16 @@ case "$choice" in
         sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
         searchsploit -h
         searchsploit -u
-        echo "Installing metasploit..."         
+        echo "Installing metasploit..."  
+        sudo apt-get install -y ruby-dev libpq-dev libpcap-dev libsqlite3-dev postgresql git ruby-bundler build-essential patch ruby-dev zlib1g-dev liblzma-dev libgmp-dev
+        sudo mkdir /opt/metasploit
+        sudo chown $USER:root -R /opt/metasploit
+        sudo chmod 770 -R /opt/metasploit
+        git clone https://github.com/rapid7/metasploit-framework.git /opt/metasploit
+        sudo chmod +x /opt/metasploit/
+        cd /opt/metasploit
+        sudo /opt/metasploit/msfupdate
+
         ;;
     "n" | "N")
         exit ;;
