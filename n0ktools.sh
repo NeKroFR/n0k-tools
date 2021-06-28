d4e41a5fbb86921e07ee80ae53b22066e516beba
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-06-28 10:47:21 +0200

    Update n0ktools.sh

--- n0ktools.sh
+++ n0ktools.sh
@@ -42,14 +42,9 @@ case "$choice" in
         searchsploit -h
         searchsploit -u
         echo "Installing metasploit..."  
-        sudo apt-get install -y ruby-dev libpq-dev libpcap-dev libsqlite3-dev postgresql git ruby-bundler build-essential patch ruby-dev zlib1g-dev liblzma-dev libgmp-dev
-        sudo mkdir /opt/metasploit
-        sudo chown $USER:root -R /opt/metasploit
-        sudo chmod 770 -R /opt/metasploit
-        git clone https://github.com/rapid7/metasploit-framework.git /opt/metasploit
-        sudo chmod +x /opt/metasploit/
-        cd /opt/metasploit
-        sudo /opt/metasploit/msfupdate
+        curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+        chmod 755 msfinstall && \
+        ./msfinstall
 
         ;;
     "n" | "N")
